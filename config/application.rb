a8bd5f3eb4f390ae956f6761a5418d03d034028f
Alabhya Jindal <52493077+alabhyajindal@users.noreply.github.com>
2023-03-11 21:21:09 +0530

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.hosts << "rails-production-9455.up.railway.app"
   end
 end
